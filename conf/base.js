59fe9a7c9c139acb6beb8b0955b2ab981fc727fb
Renat Idrisov <parsifal.47@gmail.com>
2014-08-17 01:39:01 +0700

    sample conf

---
+++ conf/base.js
@@ -0,0 +1,6 @@
+module.exports = (function () {
+    return {
+        leftBracket: "😊",
+        rightBracket: "😟"
+    }
+})();
---
+++ conf/stdlib.js
@@ -0,0 +1,18 @@
+module.exports = (function () {
+    return {
+        'defun': "⚓️",
+        'if': "😉",
+        'or': "👮",
+        'equals': "👌",
+        'morethan': "👍",
+        'lessthan': "👎",
+        'minus': "🙀",
+        'plus': "💏",
+        'lambda': "🚀",
+        'list': "🏢",
+        'quote': "🎁",
+        'eval': "💣",
+        'car': "🚗",
+        'cdr': "🚜"
+    }
+})();
